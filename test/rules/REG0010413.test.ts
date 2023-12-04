6fc14f788abd167831c8061f0c26623391af76cf
Mattia Bonardi <mattiabonardi99@gmail.com>
2023-12-04 15:21:39 +0100

    feat: align real rule and add jest for unit test

---
+++ test/rules/REG0010413.test.ts
@@ -0,0 +1,13 @@
+import { RuleVariableMap } from "../../src/types/general";
+import { REG0010413 } from "../../src/rules/REG0010413";
+
+describe("REG0010413 test", () => {
+  it("Case 1", async () => {
+    // define input
+    const input: RuleVariableMap = {};
+    // call function
+    const output = REG0010413(input);
+    // check values
+    expect(output["D§COEF"]).toBe("0");
+  });
+});
