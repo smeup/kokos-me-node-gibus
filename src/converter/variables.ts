ffe61d14e0ec84ec0f2d9d9293dd66d9aabb87eb
lanarimarco <marco.lanari@smeup.com>
2023-12-14 17:49:48 +0100

    Refactor variables class to use input and output objects in the same class

--- src/converter/variables.ts
+++ src/converter/variables.ts
@@ -2,12 +2,17 @@ import { RuleVariableMap } from "types/general";
 
 const D_DISE_LEN = 10
 
-class InputVariable {
-    ruleVariableMap: RuleVariableMap;
+/***
+ * This class is a wrapper of RuleVariableMap to expose
+ * the variables as methods
+ */
+class Variables {
 
+    input: RuleVariableMap;
+    output: RuleVariableMap = {};
 
     constructor(ruleVariableMap: RuleVariableMap) {
-        this.ruleVariableMap = ruleVariableMap
+        this.input = ruleVariableMap
     }
 
     getCF(): string {
@@ -15,11 +20,11 @@ class InputVariable {
     }
 
     getCM(): string {
-        return this.ruleVariableMap["D§COMP"]
+        return this.input["D§COMP"]
     }
 
     getCOL(): string {
-        const xconfi = this.ruleVariableMap["XCONFI"] || "";
+        const xconfi = this.input["XCONFI"] || "";
         const firstFourChars = xconfi.substring(0, 4).trim().padEnd(4, " ");
         return firstFourChars;
     }
@@ -29,7 +34,7 @@ class InputVariable {
     }
 
     getCON_B(): string {
-        const xconfi = this.ruleVariableMap["XCONFI"] || "";
+        const xconfi = this.input["XCONFI"] || "";
         const firstFourChars = xconfi.substring(4, 9).trim().padEnd(5, " ");
         return firstFourChars;
     }
@@ -39,102 +44,96 @@ class InputVariable {
     }
 
     getNT(): string {
-        return this.ruleVariableMap["D§NOTA"] || ""
+        return this.input["D§NOTA"] || ""
     }
 
     getQ1(): string {
-        return this.ruleVariableMap["D§QUA1"] || ""
+        return this.input["D§QUA1"] || ""
     }
 
     getQ2(): string {
-        return this.ruleVariableMap["D§QUA2"] || ""
+        return this.input["D§QUA2"] || ""
     }
 
     getQ3(): string {
-        return this.ruleVariableMap["D§QUA3"] || ""
+        return this.input["D§QUA3"] || ""
     }
 
     getQ4(): string {
-        return this.ruleVariableMap["D§QUA4"] || ""
+        return this.input["D§QUA4"] || ""
     }
 
     getQ5(): string {
-        return this.ruleVariableMap["D§QUA5"] || ""
+        return this.input["D§QUA5"] || ""
     }
 
     getS1(): string {
-        return this.ruleVariableMap["D§USR1"] || ""
+        return this.input["D§USR1"] || ""
     }
 
     getS2(): string {
-        return this.ruleVariableMap["D§USR2"] || ""
+        return this.input["D§USR2"] || ""
     }
 
     getLUNG(): string {
         return this.getCON_B();
     }
-}
-
-class OutputVariable {
-
-    variables: RuleVariableMap = {};
 
     setCF(cf: string) {
-        this.variables["D§COEF"] = cf
+        this.output["D§COEF"] = cf
     }
 
     setCM(cm: string) {
-        this.variables["D§COMP"] = cm
+        this.output["D§COMP"] = cm
     }
 
     setCON_A(con_a: string) {
         // D§DISE is a 10 chars len
-        this.variables["D§DISE"] = (con_a.trim().padEnd(4, " ") + (this.variables["D§DISE"] || "").substring(4)).padEnd(D_DISE_LEN, " ");
+        this.output["D§DISE"] = (con_a.trim().padEnd(4, " ") + (this.output["D§DISE"] || "").substring(4)).padEnd(D_DISE_LEN, " ");
     }
 
     setCON_B(con_a: string) {
         // D§DISE is a 10 chars len
-        this.variables["D§DISE"] = ((this.variables["D§DISE"] || "").padEnd(4, " ") + con_a.trim()).padEnd(D_DISE_LEN, " ");
+        this.output["D§DISE"] = ((this.output["D§DISE"] || "").padEnd(4, " ") + con_a.trim()).padEnd(D_DISE_LEN, " ");
     }
 
     setLG(lg: string) {
         if (lg.trim() === "" || lg === "1") {
-            this.variables["XFVALI"] = " ";
+            this.output["XFVALI"] = " ";
         }
     }
 
     setNT(nt: string) {
-        this.variables["D§NOTA"] = nt
+        this.output["D§NOTA"] = nt
     }
 
     setQ1(q: string) {
-        this.variables["D§QUA1"] = q
+        this.output["D§QUA1"] = q
     }
 
     setQ2(q: string) {
-        this.variables["D§QUA2"] = q
+        this.output["D§QUA2"] = q
     }
 
     setQ3(q: string) {
-        this.variables["D§QUA3"] = q
+        this.output["D§QUA3"] = q
     }
 
     setQ4(q: string) {
-        this.variables["D§QUA4"] = q
+        this.output["D§QUA4"] = q
     }
 
     setQ5(q: string) {
-        this.variables["D§QUA5"] = q
+        this.output["D§QUA5"] = q
     }
 
     setUSR1(usr: string) {
-        this.variables["D§USR1"] = usr
+        this.output["D§USR1"] = usr
     }
 
     setUSR2(usr: string) {
-        this.variables["D§USR2"] = usr
+        this.output["D§USR2"] = usr
     }
-
 }
 
-export { InputVariable, OutputVariable }
+export { Variables }
