e4d52e5f6aab3e962dafb3f03b922d07920d36a1
lanarimarco <marco.lanari@smeup.com>
2023-12-14 16:59:23 +0100

    Add variable test cases for converter scaffold

---
+++ test/converter/variables.test.ts
@@ -0,0 +1,14 @@
+const INPUT_VARIABLES_TEST = "InputVariable test"
+const OUTPUT_VARIABLES_TEST = "OutputVariable test"
+
+describe(INPUT_VARIABLES_TEST, () => {
+    it("Case 1", async () => {
+        expect("0").toBe("0");
+    });
+});
+
+describe(OUTPUT_VARIABLES_TEST, () => {
+    it("Case 1", async () => {
+        expect("0").toBe("0");
+    });
+});
