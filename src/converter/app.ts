d920848c5dc7d999ef817aa80068c604e96e25f5
lanarimarco <marco.lanari@smeup.com>
2023-12-15 11:01:18 +0100

    Add conversionResultDao to RuleConverterApp constructor and implement convertRules method

--- src/converter/app.ts
+++ src/converter/app.ts
@@ -1,19 +1,33 @@
-import { IRuleConverterService, IRuleDao } from "./types";
+import { IRuleConverterService, IRuleDao, IConversionResultDao } from "./types";
 
 class RuleConverterApp {
     ruleDao: IRuleDao;
     ruleConverterService: IRuleConverterService;
+    conversionResultDao: IConversionResultDao;
 
-    constructor(ruleDao: IRuleDao, ruleConverterService: IRuleConverterService) {
+    constructor(ruleDao: IRuleDao, ruleConverterService: IRuleConverterService, conversionResultDao: IConversionResultDao) {
         this.ruleDao = ruleDao;
         this.ruleConverterService = ruleConverterService;
+        this.conversionResultDao = conversionResultDao
     }
 
+    /**
+     * Converts all unconverted rules. 
+     * Process:
+     * - gets all rule to convert from @see IRuleDao.getUnconvertedRules and for each rule it invkes:
+     *   - @see IRuleConverterService.convertRule
+     *   - @see IConversionResultDao.saveConversionResult
+     *   - @see IRuleDao.saveConversionResult
+     *   - 
+    */
     convertRules(): void {
         const rules = this.ruleDao.getUnconvertedRules();
         for (const rule of rules) {
+            console.log(`${rule.id} - converting rule`);
             const result = this.ruleConverterService.convertRule(rule);
-            console.log(`Converted rule ${result.rule.id}`);
+            console.log(`${rule.id} - saving conversion result ${result.javaScript}`);
+            this.conversionResultDao.saveConversionResult(result);
+            console.log(`${rule.id} - marking rule as converted`);
             this.ruleDao.markRuleAsConverted(rule);
         }
     }
