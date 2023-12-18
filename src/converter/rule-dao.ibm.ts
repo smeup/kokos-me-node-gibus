d43938ceba72b93cfc4f1c27e3f9c9259a302c62
lanarimarco <marco.lanari@smeup.com>
2023-12-18 17:23:14 +0100

    Add doc

--- src/converter/rule-dao.ibm.ts
+++ src/converter/rule-dao.ibm.ts
@@ -2,17 +2,26 @@ import { IRuleDao } from './types';
 import { Rule } from './types';
 import * as ibm_db from 'ibm_db';
 
+/**
+ * Represents a RuleDao implementation for IBM database.
+ */
 class RuleDaoIBM implements IRuleDao {
 
     private connectionString: string;
 
+    /**
+     * Creates an instance of RuleDaoIBM.
+     * @param connectionString The connection string for the IBM database, the format is: 
+     * DATABASE=database;HOSTNAME=hostname;PORT=port;PROTOCOL=TCPIP;UID=username;PWD=password;
+     */
     constructor(connectionString: string) {
         this.connectionString = connectionString;
     }
 
-    // create a connecton string for ibmd db2 as400
-    
-
+    /**
+     * Retrieves unconverted rules from the IBM database.
+     * @returns An array of unconverted rules.
+     */
     getUnconvertedRules(): Rule[] {
         const rules: Rule[] = [];
         const conn = ibm_db.openSync(this.connectionString);
@@ -35,6 +44,10 @@ class RuleDaoIBM implements IRuleDao {
         }
     };
 
+    /**
+     * Marks a rule as converted in the IBM database.
+     * @param rule The rule to mark as converted.
+     */
     async markRuleAsConverted(rule: Rule) {
         const conn = ibm_db.openSync(this.connectionString);
         try {
