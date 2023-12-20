ccf5401b420df8e2d41a0c492f6212cbf6efd02c
lanarimarco <marco.lanari@smeup.com>
2023-12-20 12:15:38 +0100

    Implement RuleDaoExamples class with getUnconvertedRules and markRuleAsConverted methods

--- src/converter/rule-dao.examples.ts
+++ src/converter/rule-dao.examples.ts
@@ -8,14 +8,29 @@ import * as fs from 'fs'
  */
 class RuleDaoExamples implements IRuleDao {
 
+    private logPath;
+
     /**
-     * Retrieves unconverted rules.
+     * Creates a new instance of the RuleDaoExamples class.
+     * @param logPath The path where will be logged the rules when converted. Default is .work/conversion-result.txt.
+     */
+    constructor(logPath: string = path.resolve(".work", "conversion-result.txt")) {
+        this.logPath = logPath;
+        fs.mkdirSync(path.dirname(this.logPath), { recursive: true });
+        if (!fs.existsSync(this.logPath)) {
+            fs.writeFileSync(this.logPath, '', 'utf-8');
+        }
+    }
+
+    /**
+     * Retrieves unconverted rules. This method verifies the .work/conversion-result.txt file to avoid retrieving already converted rules.
      * The rules are retrieved from the /assets/test/rules.tsv file.
      * Skip the first row.
      */
     getUnconvertedRules(): Rule[] {
         const filePath = path.resolve("assets", "test", "rules.tsv");
         const rules: Rule[] = [];
+        const convertedRulesId = this.getConvertedRulesIds();
 
         const fileContent = fs.readFileSync(filePath, 'utf-8');
         const rows = fileContent.split(/\n|\r\n/);
@@ -30,6 +45,10 @@ class RuleDaoExamples implements IRuleDao {
                 continue;
             }
             const COMP = record[0];
+            if (convertedRulesId.indexOf(COMP) >= 0) {
+                console.log(`Rule ${COMP} already converted. Skipping...`);
+                continue;
+            };
             // const PRGR = parseInt(record[1]);
             const REGO = record[2].replace(/^""$/g, '');
             const IF_TRUE = record[3].replace(/^""$/g, '');
@@ -45,8 +64,32 @@ class RuleDaoExamples implements IRuleDao {
 
         return rules;
     }
+
+    /***
+     * Marks a rule as converted. The rule is written to the .work/conversion-result.txt file.
+     */
     markRuleAsConverted(rule: Rule): void {
-        throw new Error('Method not implemented.');
+        const timestamp = new Date().toISOString();
+        const row = `${rule.id}\t${timestamp}\n`;
+        fs.appendFileSync(this.logPath, row, 'utf-8');
+    }
+
+    private getConvertedRulesIds(): string[] {
+        const rulesIds: string[] = [];
+
+        const fileContent = fs.readFileSync(this.logPath, 'utf-8');
+        const rows = fileContent.split(/\n|\r\n/);
+        let rowCount = 0;
+        for (const row of rows) {
+            const record = row.split('\t');
+            if (record.length != 2) {
+                console.log(`Invalid record: ${record}`);
+                continue;
+            }
+            const COMP = record[0];
+            rulesIds.push(COMP);
+        }
+        return rulesIds;
     }
 
 }
--- test/converter/rule-dao.examples.test.ts
+++ test/converter/rule-dao.examples.test.ts
@@ -1,17 +1,28 @@
 import { RuleDaoExamples } from '../../src/converter/rule-dao.examples';
 import { Condition, Rule } from '../../src/converter/types';
+import * as path from 'path';
+import * as fs from 'fs';
+
+
+const LOG_PATH = path.resolve(".work", "conversion-result-test.txt");
 
 describe('RuleDaoExamples', () => {
     let ruleDao: RuleDaoExamples;
 
     beforeEach(() => {
-        ruleDao = new RuleDaoExamples();
+        fs.rmSync(LOG_PATH, { force: true });
+        ruleDao = new RuleDaoExamples(LOG_PATH);
     });
 
     describe('getUnconvertedRules', () => {
         it('should retrieve unconverted rules from the /assets/test/rules.tsv file', () => {
+            const unconvertedRules = ruleDao.getUnconvertedRules();
+
+            // Assert
+            expect(unconvertedRules.length).toBe(38);
+
             // Get first 2 rules
-            const result = ruleDao.getUnconvertedRules().slice(0, 2);
+            const result = unconvertedRules.slice(0, 2);
 
             // Assert
             expect(result).toEqual([
@@ -28,12 +39,31 @@ describe('RuleDaoExamples', () => {
     });
 
     describe('markRuleAsConverted', () => {
-        it('should throw an error as the method is not implemented', () => {
+        it(`should write the rule to the ${LOG_PATH} file`, () => {
             // Arrange
-            const mockRule: Rule = { id: 'COMP1', conditions: [] };
+            const mockRule: Rule = { id: 'REG0003124', conditions: [] };
 
-            // Act & Assert
-            expect(() => ruleDao.markRuleAsConverted(mockRule)).toThrow('Method not implemented.');
+            // Act
+            ruleDao.markRuleAsConverted(mockRule);
+
+            // Assert
+
+            const fileContent = fs.readFileSync(LOG_PATH, 'utf-8');
+            expect(fileContent).toContain(`${mockRule.id}\t`);
+            ruleDao.getUnconvertedRules()[0]
+        });
+        it(`REG0003124 is converted and than does not be in unconvertedRules`, () => {
+            // Arrange
+            const mockRule: Rule = { id: 'REG0003124', conditions: [] };
+
+            // Act
+            ruleDao.markRuleAsConverted(mockRule);
+
+            // Assert
+            const firsRuleId = ruleDao.getUnconvertedRules()[0].id;
+            expect(firsRuleId).not.toBe('REG0003124');
         });
     });
+
+
 });
