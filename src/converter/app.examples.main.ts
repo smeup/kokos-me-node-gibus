0fa7a5ada34b3307ef77294b93b4a3a66cfe3cf4
lanarimarco <marco.lanari@smeup.com>
2023-12-26 19:38:54 +0100

    Add main to execute RuleConverterAppExamples class

--- src/converter/app.examples.main.ts
+++ src/converter/app.examples.main.ts
@@ -3,6 +3,7 @@
 /**
  * Main function to convert rules using the RuleConverterAppExamples class.
  * @returns {Promise<void>} A promise that resolves when the rules are converted.
+ * @see RuleConverterAppExamples
  */
 import { RuleConverterAppExamples } from "./app.examples";
 
