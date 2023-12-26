3ebddfb9de0c75ad753a87d74e1ac1b5e0c5f986
lanarimarco <marco.lanari@smeup.com>
2023-12-26 12:03:56 +0100

    Restore RULE_MAPPING

--- src/types/rule.ts
+++ src/types/rule.ts
@@ -1,12 +1,8 @@
 import { REG0009397 } from "../rules/REG0009397.js";
 import { REG0010413 } from "../rules/REG0010413.js";
-import { REG0003124 } from "../rules/REG0003124";
-import { REG0003127 } from "../rules/REG0003127";
 import { RuleRegistry } from "./general.js";
 
 export const RULE_MAPPING: RuleRegistry = {
   REG0010413,
   REG0009397,
-  REG0003124,
-  REG0003127,
-};
+  };
