c53f845b76db8042e8d861acb6029213b32e3d0c
lanarimarco <marco.lanari@smeup.com>
2023-12-27 22:15:28 +0100

    Refactor test files for REG0003124 and REG0003127 rules. Before they are converted and after tested

--- test/rules/REG0003124.test.ts
+++ test/rules/REG0003124.test.ts
@@ -1,25 +1,39 @@
-import { RuleVariableMap } from "../../src/types/general";
-import { REG0003124 } from "../../src/rules/REG0003124";
+/**
+ * This file tests a rule converted from openapi.
+ */
+import { Rule, RuleVariableMap } from "../../src/types/general";
+import * as utils from "../../src/converter/utils";
 
+let rule: Rule
+
+beforeAll(async () => {
+  await utils.runFunctionIfOpenAIKeySet(async () => {
+    rule = await utils.convertExampleRule("REG0003124");
+  });
+}, 20000);
 
 describe("REG0003124", () => {
   it(`should output["XFVALI"]' undefined when §L_PORTANTE2 is greater than 0`, () => {
-    const input: RuleVariableMap = {
-      "§L_PORTANTE2": 1,
-    };
+    if (utils.isOpenAIKeySet()) {
+      const input: RuleVariableMap = {
+        "§L_PORTANTE2": 1,
+      };
 
-    const output = REG0003124(input);
+      const output = rule(input);
 
-    expect(output["XFVALI"]).toBeUndefined();
+      expect(output["XFVALI"]).toBe(undefined);
+    }
   });
 
   it(`should output["XFVALI"] = '' when §L_PORTANTE2 is less or equals  0`, () => {
-    const input: RuleVariableMap = {
-      "§L_PORTANTE2": 0,
-    };
+    if (utils.isOpenAIKeySet()) {
+      const input: RuleVariableMap = {
+        "§L_PORTANTE2": 0,
+      };
 
-    const output = REG0003124(input);
+      const output = rule(input);
 
-    expect(output["XFVALI"]).toBe('');
+      expect(output["XFVALI"]).toBe('');
+    }
   });
 });
--- test/rules/REG0003127.test.ts
+++ test/rules/REG0003127.test.ts
@@ -1,5 +1,17 @@
-import { RuleVariableMap } from "../../src/types/general";
-import { REG0003127 } from "../../src/rules/REG0003127";
+/**
+ * This file tests a rule converted from openapi.
+ */
+import { Rule, RuleVariableMap } from "../../src/types/general";
+import * as utils from "../../src/converter/utils";
+
+
+let rule: Rule;
+
+beforeAll(async () => {
+  await utils.runFunctionIfOpenAIKeySet(async () => {
+    rule = await utils.convertExampleRule("REG0003127");
+  });
+}, 20000);
 
 
 describe("REG0003172", () => {
@@ -8,44 +20,47 @@ describe("REG0003172", () => {
     // if ((vars.get('§L_PORTANTE2') > 0) && (vars.get('§L_PORTANTE3') == 0) && (vars.get('§FS_FAM2_WALL') == vars.get('§SI'))) {
     //    vars.setCF(1);
     // }
+    if (utils.isOpenAIKeySet()) {
+      const input: RuleVariableMap = {
+        "§SI": 0,
+        "§L_PORTANTE2": 1,
+        "§L_PORTANTE3": 0,
+        "§FS_FAM2_WALL": 0,
+      };
 
-    const input: RuleVariableMap = {
-      "§SI": 0,
-      "§L_PORTANTE2": 1,
-      "§L_PORTANTE3": 0,
-      "§FS_FAM2_WALL": 0,
-    };
+      const output = rule(input);
 
-    const output = REG0003127(input);
-
-    expect(output["D§COEF"]).toBe(1);
+      expect(output["D§COEF"]).toBe(1);
+    }
   });
 
   it(`§L_PORTANTE2 > 0 && §L_PORTANTE3 > 0 && §FS_FAM2_WALL == §SI`, () => {
     // if ((vars.get('§L_PORTANTE2') > 0) && (vars.get('§L_PORTANTE3') > 0) && (vars.get('§FS_FAM2_WALL') == vars.get('§SI'))) {
     //   vars.setCF(2);
     // }
+    if (utils.isOpenAIKeySet()) {
+      const input: RuleVariableMap = {
+        "§SI": 0,
+        "§L_PORTANTE2": 1,
+        "§L_PORTANTE3": 1,
+        "§FS_FAM2_WALL": 0,
+      };
 
-    const input: RuleVariableMap = {
-      "§SI": 0,
-      "§L_PORTANTE2": 1,
-      "§L_PORTANTE3": 1,
-      "§FS_FAM2_WALL": 0,
-    };
+      const output = rule(input);
 
-    const output = REG0003127(input);
-
-    expect(output["D§COEF"]).toBe(2);
+      expect(output["D§COEF"]).toBe(2);
+    }
   });
 
   it(`nothing true`, () => {
+    if (utils.isOpenAIKeySet()) {
+      const input: RuleVariableMap = {
+      };
 
-    const input: RuleVariableMap = {
-    };
+      const output = rule(input);
 
-    const output = REG0003127(input);
-
-    expect(output["D§COEF"]).toBe(0);
+      expect(output["D§COEF"]).toBe(0);
+    }
   });
 
 });
