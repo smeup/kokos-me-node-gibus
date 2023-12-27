9b6de821bc027583f06f140fdaf3d59cc633a592
lanarimarco <marco.lanari@smeup.com>
2023-12-27 22:19:24 +0100

    Refactor saveConversionResult method in ConversionResultDaoFileSystem

--- src/converter/convertion-result-dao.file.ts
+++ src/converter/convertion-result-dao.file.ts
@@ -19,11 +19,10 @@ class ConversionResultDaoFileSystem implements IConversionResultDao {
 
     saveConversionResult(result: ConversionResult): void {
         const destPath = path.resolve(this.rootPath, `${result.ruleId}.ts`);
-        if (fs.existsSync(destPath) && !this.overwrite) {
-            console.warn(`File ${destPath} already exists and overwrite is set to false.`);
-        } else {
-            fs.writeFileSync(destPath, result.javaScript);
+        if (this.overwrite) {
+            fs.rmSync(destPath, { force: true })
         }
+        fs.writeFileSync(destPath, result.javaScript);
     }
 
 }
