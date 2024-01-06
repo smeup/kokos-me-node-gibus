74133a2e3408b6755b19b247a3ae10fddd1a3f35
lanarimarco <marco.lanari@smeup.com>
2024-01-06 19:42:45 +0100

    Refactor conversion result validation process, now the file tested is never deleted

--- src/converter/conversion-result-validator.ts
+++ src/converter/conversion-result-validator.ts
@@ -10,7 +10,11 @@ class SyntaxErrorValidator implements IConversionResultValidator {
 
     /**
      * Validates a conversion result.
-     * The validation process consists of: require the converted rule and check if it throws an error.
+     * The validation process consists of create a file named <ruleId>.tmp.ts in the src/rules folder,
+     * and trying to import the rule from that file. If the import fails, the validation fails.
+     * The <ruleId>.tmp.ts is not deleted after the validation process in order to allow the developer to inspect it for debugging purposes.
+     * @param result The conversion result to validate.
+     * @throws Error if the validation fails.
      */
     validateConversionResult(result: ConversionResult): void {
         const tmpRulePath = path.resolve(process.cwd(), "src", "rules", `${result.ruleId}.tmp.ts`);
@@ -20,8 +24,6 @@ class SyntaxErrorValidator implements IConversionResultValidator {
             require(tmpRulePath)[result.ruleId] as Rule;
         } catch (error: any) {
             throw new Error(`Validation of: ${result} failed: ${error.message}`);
-        } finally {
-            fs.rmSync(tmpRulePath, { force: true });
         }
     }
 }
