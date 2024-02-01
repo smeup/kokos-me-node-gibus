75c663d4819361fff98a32df1e91581bcabe23c9
lanarimarco <marco.lanari@smeup.com>
2024-02-01 21:43:52 +0100

    Update RuleDaoProduction constructor parameters

--- src/converter/convert-production.ts
+++ src/converter/convert-production.ts
@@ -3,8 +3,9 @@ import { SyntaxErrorValidator } from "./conversion-result-validator";
 import { ConversionResultDaoFileSystem } from "./convertion-result-dao.file";
 import { OpenAIRuleConverter } from "./rule-converter.openai";
 import { RuleDaoProduction } from "./rule-dao.production";
+import * as consts from "./consts" ;
 
-const ruleDao = new RuleDaoProduction(process.env.ME_GIBUS_CONN_STRING!!);
+const ruleDao = new RuleDaoProduction(consts.host, consts.user, consts.password);
 const ruleConverterService = new OpenAIRuleConverter();
 const conversionResultValidator = new SyntaxErrorValidator();
 const conversionResultDao = new ConversionResultDaoFileSystem("src/rules");
