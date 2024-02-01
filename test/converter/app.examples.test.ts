c94d2cae611a1694943dfa8d7dfd0ac8b20ad258
lanarimarco <marco.lanari@smeup.com>
2024-02-01 18:10:24 +0100

    Fix issue where the test fails if .work directory does not contain the converted rules

--- test/converter/app.examples.test.ts
+++ test/converter/app.examples.test.ts
@@ -6,31 +6,38 @@ import * as fs from 'fs';
 const allow = (ruleId: string) => ["REG0003124", "REG0003127", "REG0008660"].includes(ruleId);
 const rootPath = path.resolve(".work", "converted-rules");
 const appExamples: RuleConverterAppExamples = new RuleConverterAppExamples({ allow: allow, rootPath: rootPath });
+let openaiKeySet: boolean = false;
 
 describe("RuleConverterAppExamples", () => {
 
     beforeAll(async () => {
         await runFunctionIfOpenAIKeySet(async () => {
+            openaiKeySet = true;
             await appExamples.convertRules();
         });
     }, 60000); // Increase timeout to 60000 seconds
 
     describe("check if rule loaded from assets is equals to rule converted", () => {
         it("REG0003124", async () => {
-
-            expect(loadRulesFromWorkConvertedRules("REG0003124")).toBe(loadRuleFromAssetAsset("REG0003124"))
+            if (openaiKeySet) {
+                expect(loadRulesFromWorkConvertedRules("REG0003124")).toBe(loadRuleFromAsset("REG0003124"))
+            }
         });
         it("REG0003127", async () => {
-            expect(loadRulesFromWorkConvertedRules("REG0003127")).toBe(loadRuleFromAssetAsset("REG0003127"))
+            if (openaiKeySet) {
+                expect(loadRulesFromWorkConvertedRules("REG0003127")).toBe(loadRuleFromAsset("REG0003127"))
+            }
         });
         it("REG0008660", async () => {
-            expect(loadRulesFromWorkConvertedRules("REG0008660")).toBe(loadRuleFromAssetAsset("REG0008660"))
+            if (openaiKeySet) {
+                expect(loadRulesFromWorkConvertedRules("REG0008660")).toBe(loadRuleFromAsset("REG0008660"))
+            }
         });
     });
 
 });
 
-function loadRuleFromAssetAsset(ruleId: string): string {
+function loadRuleFromAsset(ruleId: string): string {
     const filePath = path.resolve("assets", "test", "converted-rules", `${ruleId}.ts`);
     return removeUnnecessaryChars(fs.readFileSync(filePath, { encoding: "utf-8" }));
 }
