e9e45c38bb287d8653e222244ae3d94767a8bfcc
lanarimarco <marco.lanari@smeup.com>
2024-02-01 21:43:43 +0100

    Add consts.ts file with environment variables

---
+++ src/converter/consts.ts
@@ -0,0 +1,5 @@
+const user = process.env.ME_GIBUS_USER || "setme";
+const password = process.env.ME_GIBUS_PASSWORD || "setme";
+const host = process.env.ME_GIBUS_HOST || "setme";
+
+export { user, password, host };
