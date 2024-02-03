355c829f13d2113fbe304b7f2a5a94c49c228b95
lanarimarco <marco.lanari@smeup.com>
2024-02-03 19:33:21 +0100

    Add node-jt400 connection and update query in RuleDaoProduction

--- test/converter/rule-dao.production-db.test.ts
+++ test/converter/rule-dao.production-db.test.ts
@@ -1,28 +1,14 @@
-import { RuleDaoProduction } from '../../src/converter/rule-dao.production';
+import { RuleDaoProduction, Row } from '../../src/converter/rule-dao.production';
 import { Condition, Rule } from '../../src/converter/types';
 import * as consts from '../../src/converter/consts';
 
-const GIBUS_RULES_ROWS = [
+const GIBUS_RULES_ROWS: Row[] = [
   {
-    COMP: 'TEST1',
+    COMP: 'REG0003124',
     PRGR: 1,
-    REGO: 'IF_TRUE1',
-    IF_TRUE: 'IF_FALSE1',
-    IF_FALSE: 'IF_FALSE1',
-  },
-  {
-    COMP: 'TEST',
-    PRGR: 2,
-    REGO: 'IF_TRUE1_2',
-    IF_TRUE: 'IF_FALSE1_2',
-    IF_FALSE: 'IF_FALSE1_3',
-  },
-  {
-    COMP: 'TEST2',
-    PRGR: 1,
-    REGO: 'IF_TRUE2',
-    IF_TRUE: 'IF_FALSE2',
-    IF_FALSE: 'IF_FALSE2',
+    REGO: '§L_PORTANTE2 > #0',
+    IF_TRUE: '',
+    IF_FALSE: `*SET *LG=''`,
   }
 ];
 
@@ -31,19 +17,18 @@ const GR = GIBUS_RULES_ROWS;
 
 const RULES: Rule[] = [
   new Rule(GR[0].COMP, [
-    new Condition(GR[0].REGO, GR[0].IF_TRUE, GR[0].IF_FALSE),
-    new Condition(GR[1].REGO, GR[1].IF_TRUE, GR[1].IF_FALSE)
-  ]),
-  new Rule(GR[2].COMP, [new Condition(GR[2].REGO, GR[2].IF_TRUE, GR[2].IF_FALSE)]),
+    new Condition(GR[0].REGO, GR[0].IF_TRUE, GR[0].IF_FALSE)
+  ])
 ];
 
 
 describe('RuleDaoProduction - db', () => {
   let ruleDao: RuleDaoProduction;
 
-  beforeEach(() => {
+  beforeEach(async () => {
     // Initialize RuleDaoProduction with test configuration
-    ruleDao = new RuleDaoProduction(consts.host, consts.user, consts.password);
+    ruleDao = new RuleDaoProduction(consts.host, consts.user, consts.password, "COMP in ('REG0003124')");
+    await ruleDao.markRuleAsNotConverted(RULES[0], 'Test error message');
   });
 
   afterEach(async () => {
@@ -51,22 +36,22 @@ describe('RuleDaoProduction - db', () => {
     await ruleDao.close();
   });
 
-
   describe('getUnconvertedRules', () => {
-    it('should retrieve unconverted rules from the database', async () => {
+    it('should retrieve unconverted rules from the database', async() => {
 
       let expected: Rule[] = RULES;
 
       // Call the method under test
-      const unconvertedRules = await ruleDao.getUnconvertedRules();
-
+      let unconvertedRules = await ruleDao.getUnconvertedRules();
+      
       // Assert the result
       expect(unconvertedRules).toEqual(expected);
+
     });
   });
 
   describe('markRuleAsConverted', () => {
-    xit('should mark a rule as converted in the database', async () => {
+    xit('should mark a rule as converted in the database',  async () => {
 
       // Create a test rule
       const rule = RULES[0];
@@ -74,8 +59,9 @@ describe('RuleDaoProduction - db', () => {
       // Call the method under test
       await ruleDao.markRuleAsConverted(rule);
 
-      // Assert that the database update method was called with the correct query
-      //expect(POOL.update).toHaveBeenCalledWith(expect.stringContaining(`UPDATE GIBUS_CONV_STATUS SET STATUS = 'P'`));
+      let unconvertedRules = await ruleDao.getUnconvertedRules();
+      expect(unconvertedRules).toEqual([]);
+      
     });
   });
 
