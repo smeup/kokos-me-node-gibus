937c557be65b2a1a83a0dc973022020f228938b4
lanarimarco <marco.lanari@smeup.com>
2024-02-19 15:03:15 +0100

    Refactor conditional statements in REG0003598.ts

--- src/rules/REG0003598.ts
+++ src/rules/REG0003598.ts
@@ -75,22 +75,25 @@ export const REG0003598: Rule = (iv) => {
     }
     
     //#4
-    if ((vars.get('§FS_MED_ACCOPP') == vars.get('§SI')) && ((vars.get('§FS_ACC_CEN') == vars.get('§SI')) || (vars.get('§FS_ACC_FIN') == vars.get('§SI')) && (vars.get('§FS_MON_ANT1') == vars.get('§SI'))) {
+    //                       ( §FS_MED_ACCOPP=§SI )     AND              ( ( §FS_ACC_CEN = §SI )          OR ( §FS_ACC_FIN = §SI ) )                                     AND ( §FS_MON_ANT1=§SI )
+    if ((vars.get('§FS_MED_ACCOPP') == vars.get('§SI')) && ((vars.get('§FS_ACC_CEN') == vars.get('§SI')) || (vars.get('§FS_ACC_FIN') == vars.get('§SI'))) && (vars.get('§FS_MON_ANT1') == vars.get('§SI'))) {
         vars.setCF(vars.get('§NR_MON_MAN'));
     }
     
     //#5
-    if ((vars.get('§FS_ACC_CEN') == vars.get('§SI')) && (((vars.get('§FS_ACC_ANT_SX') != vars.get('§SI')) && (vars.get('§FS_MON_ANT1') != vars.get('§SI')) || ((vars.get('§FS_ACC_ANT_DX') != vars.get('§SI')) && (vars.get('§FS_MON_ANT3') != vars.get('§SI')))) && (vars.get('§NR_MON_MAN') == 1)) {
+    //                  ( §FS_ACC_CEN=§SI )         AND ( (( §FS_ACC_ANT_SX<>§SI )                        AND   ( §FS_MON_ANT1<>§SI ))                      OR               ( ( §FS_ACC_ANT_DX<>§SI )          AND               (§FS_MON_ANT3<>§SI ) ) )         AND ( §NR_MON_MAN=#1 )
+    if ((vars.get('§FS_ACC_CEN') == vars.get('§SI')) && (((vars.get('§FS_ACC_ANT_SX') != vars.get('§SI')) && (vars.get('§FS_MON_ANT1') != vars.get('§SI'))) || ((vars.get('§FS_ACC_ANT_DX') != vars.get('§SI')) && (vars.get('§FS_MON_ANT3') != vars.get('§SI')))) && (vars.get('§NR_MON_MAN') == 1)) {
         vars.setCF(1);
     }
-    
+
     //#6
     if ((vars.get('§FS_ACC_CEN') == vars.get('§SI')) && (vars.get('§FS_ACC_ANT_SX') != vars.get('§SI')) && (vars.get('§FS_MON_ANT1') != vars.get('§SI')) && (vars.get('§FS_ACC_ANT_DX') != vars.get('§SI')) && (vars.get('§FS_MON_ANT3') != vars.get('§SI'))) {
         vars.setCF(2);
     }
     
     //#7
-    if ((vars.get('§FS_ACC_CEN') == vars.get('§SI')) && (((vars.get('§FS_ACC_ANT_SX') != vars.get('§SI')) && (vars.get('§FS_MON_ANT1') != vars.get('§SI')) || ((vars.get('§FS_ACC_ANT_DX') != vars.get('§SI')) && (vars.get('§FS_MON_ANT3') != vars.get('§SI')))) && (vars.get('§NR_MON_MAN') > 1)) {
+    //               ( §FS_ACC_CEN=§SI )            AND ( (( §FS_ACC_ANT_SX<>§SI )                        AND ( §FS_MON_ANT1<>§SI ))                        OR            (( §FS_ACC_ANT_DX<>§SI )              AND                   ( §FS_MON_ANT3<>§SI )) )     AND      ( §NR_MON_MAN >#1 )
+    if ((vars.get('§FS_ACC_CEN') == vars.get('§SI')) && (((vars.get('§FS_ACC_ANT_SX') != vars.get('§SI')) && (vars.get('§FS_MON_ANT1') != vars.get('§SI'))) || ((vars.get('§FS_ACC_ANT_DX') != vars.get('§SI')) && (vars.get('§FS_MON_ANT3') != vars.get('§SI')))) && (vars.get('§NR_MON_MAN') > 1)) {
         vars.setCF(vars.get('§NR_MON_MAN'));
     }
     
