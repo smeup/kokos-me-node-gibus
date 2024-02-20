a9065c6e03eb0c355129f1e0839058dd1d036ad7
lanarimarco <marco.lanari@smeup.com>
2024-02-20 12:23:14 +0100

    Add REG0000717 test

---
+++ test/rules/REG0000717.test.ts
@@ -0,0 +1,22 @@
+import { RuleVariableMap } from "../../src/types/general";
+import { REG0000717 } from "../../src/rules/REG0000717";
+import * as fs from 'fs';
+import { loadVariables } from "../../src/converter/utils";
+
+describe("REG0000717 test", () => {
+    it("Expected D§COEF= -0.028", async () => {
+        // define input
+        const input: RuleVariableMap = loadVariables(
+            fs.readFileSync(__dirname + "/REG0000717.1.json", "utf-8"),
+            (notDefaultVars) => console.log(notDefaultVars)
+        );
+
+        // call function
+        const output = REG0000717(input);
+
+        // check values
+        expect(output["D§COEF"].toFixed(3)).toBe("-0.028");
+    });
+});
+
+
