e41864c14701152f2f63cceb2f65e0d1cf33e042
lanarimarco <marco.lanari@smeup.com>
2024-02-28 21:20:38 +0100

    Revert "Commented out type provider initialization and added new file to tsconfig.json"
    This reverts commit 896c2489b2a0fb282ccf3bff5a1b56aa68a35305.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import { startServer, LOGGER } from "@sme.up/kokos-sdk-node";
-// import { setTypeProvider, getDbAccessConfig } from "./converter/utils.js";
-// import { TypeProviderDb } from "./converter/type-provider.db.js";
+import { setTypeProvider, getDbAccessConfig } from "./converter/utils.js";
+import { TypeProviderDb } from "./converter/type-provider.db.js";
 
 
-// LOGGER.info("Creating type provider...");
-// const dbAccessConfig = getDbAccessConfig();
-// if (dbAccessConfig) {
-//     LOGGER.info("Initing TypeProviderDb db...");
-//     let typeProvider = new TypeProviderDb(dbAccessConfig);
-//     await typeProvider.init();
-//     setTypeProvider(typeProvider);
-// } else {
-//     LOGGER.info("Database access configuration not provided I am going to use default type provider");
-// }
+LOGGER.info("Creating type provider...");
+const dbAccessConfig = getDbAccessConfig();
+if (dbAccessConfig) {
+    LOGGER.info("Initing TypeProviderDb db...");
+    let typeProvider = new TypeProviderDb(dbAccessConfig);
+    await typeProvider.init();
+    setTypeProvider(typeProvider);
+} else {
+    LOGGER.info("Database access configuration not provided I am going to use default type provider");
+}
 startServer("me-node-gibus");
