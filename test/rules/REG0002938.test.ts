a7b5e3dfa7fd15ffb36b485cc90b4cdb7b451a19
lanarimarco <marco.lanari@smeup.com>
2024-02-29 10:28:30 +0100

    Add REG0002938 rule and test

---
+++ test/rules/REG0002938.test.ts
@@ -0,0 +1,19 @@
+import { RuleVariableMap } from "../../src/types/general";
+import { REG0002938 } from "../../src/rules/REG0002938";
+import { loadVariables } from "../../src/converter/utils";
+import fs from 'fs';
+
+describe("REG0002938 test", () => {
+    
+
+    it("Expected D§DISE: P05502630 ", async () => {
+        // define input
+        const input: RuleVariableMap = loadVariables(fs.readFileSync(__dirname + "/REG0002938.1.json", "utf-8"));
+        // apply rule
+        const output = REG0002938(input);
+        // check 
+        expect(output["D§DISE"]).toBe("P05502630");
+    });
+
+
+});
