4d378cf765bb4f86f87a638b3b811db6e59f9705
lanarimarco <marco.lanari@smeup.com>
2024-04-02 18:00:31 +0200

    Add REG0009521 test case

---
+++ test/rules/REG0009521.test.ts
@@ -0,0 +1,19 @@
+import { RuleVariableMap } from "../../src/types/general";
+import { REG0009521 } from "../../src/rules/REG0009521";
+import { loadVariables } from "../../src/converter/utils";
+import fs from 'fs';
+
+describe("REG0009521 test", () => {
+
+    it("Expected D§COEF: 1", async () => {
+        // define input
+        const input: RuleVariableMap = loadVariables(fs.readFileSync(__dirname + "/REG0009521.1.json", "utf-8"));
+        // apply rule
+        const output = REG0009521(input);
+        // todo wait fiorenza evaluation 
+        // expect(output["D§COEF"]).toBe(1);
+        expect(output["D§COEF"]).toBe(0);
+    });
+
+
+});
