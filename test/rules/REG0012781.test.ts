df53f8c3b46850b48d7eeb75710d52d2f624c1f7
lanarimarco <marco.lanari@smeup.com>
2024-04-03 13:10:09 +0200

    Add REG0012781 rule and test

---
+++ test/rules/REG0012781.test.ts
@@ -0,0 +1,20 @@
+import { RuleVariableMap } from "../../src/types/general";
+import { REG0012781 } from "../../src/rules/REG0012781";
+import { loadVariables } from "../../src/converter/utils";
+import fs from 'fs';
+
+describe("REG0012781 test", () => {
+    
+
+    it("Expected D§USR2: 50000", async () => {
+        // define input
+        const input: RuleVariableMap = loadVariables(fs.readFileSync(__dirname + "/REG0012781.1.json", "utf-8"));
+        // apply rule
+        const output = REG0012781(input);
+        console.log(output);
+        // check 
+        expect(output["D§USR2"]).toBe(50000);
+    });
+
+
+});
