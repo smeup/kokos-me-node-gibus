bea428ae01f578616035080227bec66fa3df4db4
lanarimarco <marco.lanari@smeup.com>
2024-04-05 17:51:00 +0200

    Add new rule implementations

---
+++ src/rules/REG0000025.ts
@@ -0,0 +1,32 @@
+/**
+ * This rule represents a template implementation of a rule.
+ * It takes an input value and performs some operations on it using the Variables class.
+ * The result is returned as the output value.
+ * 
+ * @param iv The input value for the rule.
+ * @returns The output value after applying the rule.
+ */
+import { Rule } from "../types/general.js";
+import { Variables } from "../converter/variables.js";
+
+export const REG0000025: Rule = (iv) => {
+
+    const vars = new Variables(iv);
+
+    // GENERATED
+    // RULE: REG0000025
+    // REQUEST:
+    // """
+    // #1
+    // COND:
+    // sdfasdfadsf
+    // """
+    // RESPONSE:
+    
+    //#1
+    if (vars.get('sdfasdfadsf')) {
+    }
+    // GENERATED
+
+    return vars.output;
+};
