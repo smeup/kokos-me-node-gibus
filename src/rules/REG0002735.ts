057de563e7e5a998822df5f049c6f70cf4e88c38
lanarimarco <marco.lanari@smeup.com>
2024-04-09 11:26:30 +0200

    fix *var expression rule

--- src/rules/REG0002735.ts
+++ src/rules/REG0002735.ts
@@ -25,9 +25,10 @@ export const REG0002735: Rule = (iv) => {
     // """
     // RESPONSE:
     //#1
-    if (getCOL() == '0000') {
+    if (vars.getCOL() == '0000') {
         vars.setLG('');
-    }
+    } 
+    
     // GENERATED
 
     return vars.output;
