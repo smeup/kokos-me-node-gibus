8c9df6a7bbd01dd5779d1c9535cde6de9ed68483
alessandro.danzo <alessandro.danzo@gibus.it>
2024-09-11 17:00:39 +0200

    aggiunti alcuni test

---
+++ src/rules/REG0009424_gibus.ts
@@ -0,0 +1,34 @@
+/**
+ * This rule represents a template implementation of a rule.
+ * It takes an input value and performs some operations on it using the Variables class.
+ * The result is returned as the output value.
+ * 
+ * @param iv The input value for the rule.
+ * @returns The output value after applying the rule.
+ */
+import { Rule } from "../types/general.js";
+import { Variables } from "../converter/variables.js";
+
+export const REG0009424: Rule = (iv) => {
+
+    const vars = new Variables(iv);
+
+    // GENERATED
+    // RULE: REG0009424
+    // REQUEST:
+    // """
+    // #1
+    // THEN:
+    // *SET *CF = [(§N_MODULI + #1) * (§NR_FRANGI + #2 - §NR_FRANGI_LED)]
+    // """
+    // RESPONSE:
+    //#1
+    let nModuli = parseInt(vars.get('§N_MODULI'));
+    let nrFrangi = vars.get('§NR_FRANGI');
+    let nrFrangiLed = vars.get('§NR_FRANGI_LED');
+    let result = (nModuli + 1) * ( nrFrangi + 2 - nrFrangiLed);
+
+    vars.setCF(result);
+    // GENERATED
+    return vars.output;
+};
---
+++ test/rules/REG0009364.test.ts
@@ -0,0 +1,20 @@
+import { RuleVariableMap } from "../../src/types/general";
+import { REG0009364 } from "../../src/rules/REG0009364_gibus";
+import * as fs from 'fs';
+import { loadVariables } from "../../src/converter/utils";
+
+describe("REG0009364 test", () => {
+    it("Expected D§USR2=2262", async () => {
+
+        // // define input
+        const input: RuleVariableMap = loadVariables(fs.readFileSync(__dirname + "/REG0009364.1.json", "utf-8"));
+
+        // // call function
+        const output = REG0009364(input);
+
+        // // check values
+        expect(output["D§USR2"]).toBe(2262);
+    });
+});
+
+
